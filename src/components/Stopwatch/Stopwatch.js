bdf44b41b1bb82e92a6d02592815e0f0c1c791d4
Poly Goveia <poly.goveia@gmail.com>
2019-04-08 16:40:55 -0700

    add Stopwatch stop and start functionality

--- src/components/Stopwatch/Stopwatch.js
+++ src/components/Stopwatch/Stopwatch.js
@@ -2,12 +2,45 @@ import React, { Component } from 'react'
 
 class Stopwatch extends Component {
 
+    state = {
+        isRunning: false,
+        elapseTime: 0,
+        previousTime: 0
+    };
+
+    componentDidMount() {
+        this.intervalID = setInterval(() => this.tick(), 100);
+    }
+
+    tick = () => {
+        if (this.state.isRunning) {
+            const now = Date.now();
+            this.setState( prevState => ({
+                previousTime: now,
+                elapseTime: prevState.elapseTime + (now - this.state.previousTime) 
+            }));
+        }
+    }
+
+    handleStopwatch = () => {
+        this.setState( prevState => ({
+            isRunning: !prevState.isRunning
+        }));
+        if (!this.state.isRunning) {
+            this.setState({ previousTime: Date.now() });
+        }
+    }
+
     render() {
         return (
             <div className="stopwatch">
                 <h2>Stopwatch</h2>
-                <span className="stopwatch-time">0</span>
-                <button>Start</button>
+                <span className="stopwatch-time">
+                {Math.floor(this.state.elapseTime / 1000)}
+                </span>
+                <button onClick={this.handleStopwatch}>
+                    {this.state.isRunning ? 'Stop': 'Start'}
+                </button>
                 <button>Reset</button>
             </div>
         );
