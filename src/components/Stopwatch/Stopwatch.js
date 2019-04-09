dd2dad435cb2ff932c349918184e56060b44b051
Poly Goveia <poly.goveia@gmail.com>
2019-04-08 17:03:46 -0700

    fix typo

--- src/components/Stopwatch/Stopwatch.js
+++ src/components/Stopwatch/Stopwatch.js
@@ -12,7 +12,7 @@ class Stopwatch extends Component {
         this.intervalID = setInterval(() => this.tick(), 100);
     }
 
-    componentWillMount() {
+    componentWillUnmount() {
         clearInterval(this.intervalID);
     }
 
