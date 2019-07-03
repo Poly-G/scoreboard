6aa9a9d7b33e5b7922791da1ed5dab647a246a69
Poly Goveia <poly.goveia@gmail.com>
2019-07-02 20:44:15 -0700

    change hard coded names

--- src/App.js
+++ src/App.js
@@ -5,90 +5,82 @@ import Header from './components/Header/Header';
 import Player from './components/Player/Player';
 import AddPlayerForm from './components/AddPlayerForm/AddPlayerForm';
 
-
-const players = [{
-    name: "Devin",
+const players = [
+  {
+    name: 'Devin',
     id: 1,
     score: 0
   },
   {
-    name: "Lauren",
+    name: 'Juliana',
     id: 2,
     score: 0
   },
   {
-    name: "Poly",
+    name: 'Poly',
     id: 3,
     score: 0
   },
   {
-    name: "James",
+    name: 'James',
     id: 4,
     score: 0
   }
-]
-
+];
 
 class App extends Component {
+  state = {
+    players: players
+  };
 
+  handleScoreChange = (index, delta) => {
+    this.setState(prevState => ({
+      score: (prevState.players[index].score += delta)
+    }));
+  };
 
-state = {
-  players: players
-}
+  handleAddPlayer = name => {
+    this.setState(prevState => {
+      return {
+        players: [
+          ...prevState.players,
+          {
+            name: name,
+            score: 0,
+            id: Date.now()
+          }
+        ]
+      };
+    });
+  };
 
-   handleScoreChange = (index, delta) => {
-     this.setState( prevState => ({
-       score: prevState.players[index].score += delta
-     }))
-   };
-
-handleAddPlayer = name => {
-  this.setState( prevState => {
-    return {
-      players: [
-        ...prevState.players,
-        {
-          name: name,
-          score: 0,
-          id: Date.now()
-        }
-      ]
-    };
-  });
-}
-
-handleRemovePlayer = (id) => {
-  this.setState( prevState => {
-    return {
-      players: prevState.players.filter( p => p.id !== id )
-    }
-  })
-}
+  handleRemovePlayer = id => {
+    this.setState(prevState => {
+      return {
+        players: prevState.players.filter(p => p.id !== id)
+      };
+    });
+  };
 
   render() {
     return (
-     <div className="scoreboard">
-      <Header 
-       title="Scoreboard" 
-       players={this.state.players}
-       />
-    
-    
-    {/* Players List*/}
-    {this.state.players.map( (player, index) =>
-      <Player 
-        name={player.name} 
-        id={player.id}
-        key={player.id.toString()}
-        removePlayer={this.handleRemovePlayer}
-        score={player.score}
-        changeScore={this.handleScoreChange}
-        index={index}
-       />
-      
-    )}
-    <AddPlayerForm addPlayer={this.handleAddPlayer}/>
-    </div>
+      <div className="scoreboard">
+        <Header title="Scoreboard" players={this.state.players} />
+
+        {/* Players List*/}
+        {this.state.players.map((player, index) => (
+          <Player
+            name={player.name}
+            id={player.id}
+            key={player.id.toString()}
+            removePlayer={this.handleRemovePlayer}
+            score={player.score}
+            changeScore={this.handleScoreChange}
+            index={index}
+          />
+        ))}
+        <AddPlayerForm addPlayer={this.handleAddPlayer} />
+      </div>
     );
   }
 }
